f759e2092326d62872d711bed716c577bba6474a
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-02-09 16:38:11 -0300

    Adição da funcionalidade de Refresh

--- main.go
+++ main.go
@@ -12,16 +12,12 @@ func get(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "get called"}`))
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
-}
-
 func main() {
 	r := mux.NewRouter()
 	//api := r.PathPrefix("/").Subrouter()
 	r.HandleFunc("/login", login.LoginHandler).Methods(http.MethodPost)
+	r.HandleFunc("/refresh", login.RefreshHandler).Methods(http.MethodPost)
+
 	r.HandleFunc("/", get).Methods(http.MethodGet)
 	http.ListenAndServe(":8080", r)
 
